6ba1570bffcd91ceaa43ffac689452ecab6dd5d7
Balazs Benedek <bbenedek@skawa.hu>
2018-10-03 21:08:37 +0200

    Node8 2GB deployment to live Firebase environment

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,10 @@
 const functions = require('firebase-functions');
 const expressApp = require('./lib/urlAnalyzerApp');
 
+const opts = {memory: '2GB', timeoutSeconds: 60};
+
 // forward requsts to the express app
-const analyzerEndpoints = functions.https.onRequest(expressApp.urlAnalyzerApp);
+const analyzerEndpoints = functions.runWith(opts).https.onRequest(expressApp.urlAnalyzerApp);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
